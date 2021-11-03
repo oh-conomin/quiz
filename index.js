703d15ae4bfbb09c6fa315785a259a08fb740adf
oh-conomin <ddyu_yubb@yahoo.co.jpclear>
2021-11-03 15:33:10 +0900

    in

--- index.js
+++ index.js
@@ -1,23 +1,30 @@
 //const table=document.getElementById("table");
 const li = document.createElement("li");
 const ul = document.getElementById("ul");
-const text=document.createElement("div");
+const text = document.createElement("div");
+const an = document.getElementById("an");
 
-li.innerText = "問題";
+const que={
+  q1:"日本の首都は",
+  a1:"東京"
+};
+
+
+li.innerText = que.q1;
 ul.appendChild(li);
 const btn = document.createElement("button");
-btn.innerText = "解答";
+btn.innerText ="答えを表示";
 li.appendChild(btn);
 
-btn.addEventListener("click", function (e) {
-  const ans = document.createElement("p");
+btn.addEventListener(
+  "click",
+  function (e) {
+    e.preventDefault();
+    const ans = document.createElement("p");
 
-  ans.innerText="答え";
-  ul.appendChild(text);
-  text.appendChild(ans);
-  
-  
- 
-},
-{ once: true 
-} );
+    ans.innerText = an.value;
+    ul.appendChild(text);
+    text.appendChild(ans);
+    an.value="";
+  },{ once: true
+   });
