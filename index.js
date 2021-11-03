dddf23c76e8f37f108732175ef799757a90a10a4
oh-conomin <ddyu_yubb@yahoo.co.jpclear>
2021-11-03 23:40:35 +0900

    displayl

--- index.js
+++ index.js
@@ -1,30 +1,25 @@
-//const table=document.getElementById("table");
 const li = document.createElement("li");
 const ul = document.getElementById("ul");
 const text = document.createElement("div");
-const an = document.getElementById("an");
+const btn = document.createElement("button");
 
-const que={
-  q1:"日本の首都は",
-  a1:"東京"
+const que = {
+  q1: "日本の首都は",
+  a1: "東京",
 };
-
 
 li.innerText = que.q1;
 ul.appendChild(li);
-const btn = document.createElement("button");
-btn.innerText ="答えを表示";
+btn.innerText = "答えを表示";
 li.appendChild(btn);
 
-btn.addEventListener(
-  "click",
-  function (e) {
-    e.preventDefault();
-    const ans = document.createElement("p");
+const ans = document.createElement("p");
 
-    ans.innerText = an.value;
-    ul.appendChild(text);
-    text.appendChild(ans);
-    an.value="";
-  },{ once: true
-   });
+ans.innerText = que.a1;
+
+btn.addEventListener("click", function (e) {
+  e.preventDefault();
+  text.classList.toggle("no");
+  ul.appendChild(text);
+  text.appendChild(ans);
+});
